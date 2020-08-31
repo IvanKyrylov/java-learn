d64f54cd5831e4f0caec90199713ef282da41610
Ivan Kyrylov <vannsha.may@gmail.com>
2020-08-31 14:08:46 +0300

    Create <README.md> and writing task description

--- hw01-gradle/src/main/java/hw01/gradle/HelloGuava.java
+++ hw01-gradle/src/main/java/hw01/gradle/HelloGuava.java
@@ -4,7 +4,7 @@ import com.google.common.math.IntMath;
 
 public class HelloGuava {
     public static void callMethodCheckedMultiplyGuava() {
-        int mustNotOverflow = IntMath.checkedMultiply(5, 0);
+        int mustNotOverflow = IntMath.checkedMultiply(5, 5);
         System.out.println(mustNotOverflow);
     }
 }
